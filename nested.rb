ac6ecd5b6dff288a54f536af260de3dc676e20f9
Leah Boyd <leahboyd08@gmail.com>
2019-03-25 18:11:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,8 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     dennis_new_info = "Assembly"	
-      programmer_hash[:dennis_ritchie][:languages] = dennis_new_info
+      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
     programmer_hash 
 
 end
